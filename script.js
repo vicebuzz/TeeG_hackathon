59c0a54a884b4773a51848ce401356a8ca47bd90
Trezeno <113196678+Trezeno@users.noreply.github.com>
2023-11-06 23:06:35 +0000

    Multiple changes (see desc)
    HTML: Modified AI generated images list so you can select current user (free/prem) and display correct information related to current user
    
    JS: Added function to select and highlight an image, function to remove all highlighted, function to show correct AI generated images depending on user
    
    CSS: Stylised things better visually for debugging, as well as completing in-line boxes of generated ai images

--- script.js
+++ script.js
@@ -1,5 +1,59 @@
-//Function to show a list of generated images
-function showGenImages() {
-  let genImages = document.getElementById("generated-images");
-  genImages.removeAttribute("hidden");
+//Function to change selected user
+function changeUser(e) {
+  let selectedUserValue = e.value;
+  const selectMenu = document.getElementById("freePremUserSelect");
+  const options = selectMenu.options;
+  if (selectedUserValue == 0) {
+    options[0].id = "selectedUser";
+    options[1].id = "";
+  } else {
+    options[1].id = "selectedUser";
+    options[0].id = "";
+  }
+  console.log("User changed to:", document.getElementById("selectedUser").innerHTML);
 }
+
+//Function to show a list of generated images
+function showGenImages(user) {
+  removeAllHighlighted();
+  selectedUser = user.innerHTML;
+  document.getElementById("generated-images-free").hidden = false;
+  if (selectedUser == "PREMIUM") {
+    document.getElementById("generated-images-prem").hidden = false;
+    console.log("PREMIUM generated images shown");
+  } else {
+    document.getElementById("generated-images-prem").hidden = true;
+    console.log("FREE generated images shown");
+  }
+}
+
+//Function to clear anything currently highlighted
+function removeAllHighlighted() {
+  const elements = document.querySelectorAll('.highlighted');
+  elements.forEach((el) => {
+    el.classList.remove('highlighted');
+  });
+}
+
+//Function to highlight the selected image you want
+function selectGenImage(e) {
+  // Remove the 'highlighted' class from all elements
+  removeAllHighlighted();
+
+  // Add the 'highlighted' class to the selected element
+  e.classList.add('highlighted');
+  console.log("Item highlighted");
+}
+
+//Function that will be used to call Teemill API and send selected image to t-shirt preview and grab that to send back
+function sendImageToAPI() {
+  let imageURL = 'images/bird.png'; //This will be changed later to grab whatever URL link has been used
+  let tee = document.getElementById("generated-image-selected");
+  console.log("Sending t-shirt: ");
+}
+
+
+
+
+
+
