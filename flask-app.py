ddc086c965374e0c57a322971dc9bb337d9bbbf1
vik_lav <iborn16.04.2015@gmail.com>
2023-11-07 12:52:30 +0000

    api to get t shirt designs from teemill api

---
+++ flask-app.py
@@ -0,0 +1,16 @@
+from teemil_api import TeemilAPI
+from flask import Flask, request, jsonify
+
+app = Flask('teemill-api-app')
+
+
+api = TeemilAPI()
+
+@app.route('/generate-t-shirt', methods = ['POST'])
+def generateTShirt():
+    
+    data = request.json
+
+    returnJSON = api.createImageTshirt(data['url']) 
+    
+    return returnJSON
---
+++ teemil_api.py
@@ -0,0 +1,50 @@
+import requests
+import urllib
+import base64
+
+
+
+
+class TeemilAPI():
+
+    def __init__(self):
+
+        self.publicSafeKey = 'elCp6zR3XcWXOE49c2EIoLc0yJleaJdJ3LMrBRrX'
+        self.privateKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ0ZWVtaWxsLmNvbSIsIm5iZiI6MTY5OTI4MzI2OCwiaWF0IjoxNjk5MjgzMjY4LCJzdWIiOjMyNDMzMiwiZXhwIjo5OTk5OTk5OTk5OTksInV1aWQiOiI2NTQ5MDE0NDJlMjU4IiwidG9rZW5faWQiOjY3MSwiY3JlYXRlZF9hdCI6MTY5OTI4MzI2OH0.RSLAIp_6aCsZ3FiRDLSbRuy4J2t6KQ-ZPS6OxcwxkqM'
+
+    def createImageTshirt(self, img_url):
+
+        img = self.encodeImageBase64(img_url)
+
+        url = 'https://teemill.com/omnis/v3/product/create'
+
+        payload = {
+            "image_url": f"data:image/png;base64, {str(img)[2:]}"
+            }
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer Bearer {self.publicSafeKey}"
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+
+        return response.json()
+    
+    def encodeImageBase64(self, img_url):
+
+        r = requests.get(img_url)
+
+        return base64.b64encode(r.content)
+    
+    def decodeImageBase64(self, base64_):
+
+        with open('trash/temp.png', 'wb') as tempImgFile:
+            tempImgFile.write(base64.b64decode(base64_))
+    
+
+if __name__ == '__main__':
+    api = TeemilAPI()
+    #api.decodeImageBase64(api.encodeImageBase64('https://avatars.githubusercontent.com/u/43704805?s=80&v=4'))
+    print(api.createImageTshirt('https://d1unuvan7ts7ur.cloudfront.net/0x600/filters:strip_exif()/4be698c3-af97-46b4-b98f-280af75b8da7/01GTWDTCEB3K9TEXY1B5YXQGZH'))
+    #print(str(api.encodeImageBase64('https://avatars.githubusercontent.com/u/43704805?s=80&v=4'))[2:])
